7f45c9c81d8bc3af659e23c73fa6c212451a0df2
lfrovaris <leo_rovaris@hotmail.com>
2018-06-13 23:10:40 -0300

    add viewes avaliacao (falta editar)

--- Nutris/Nutris/Controllers/AvaliacaoController.cs
+++ Nutris/Nutris/Controllers/AvaliacaoController.cs
@@ -6,32 +6,8 @@ using System.Web.Mvc;
 
 namespace Nutris.Controllers
 {
-    public class HomeController : Controller
+    public class AvaliacaoController : Controller
     {   
-        [Authorize]
-        public ActionResult IndexNutri()
-        {
-            return View();
-        }
-
-        [Authorize]
-        public ActionResult IndexCliente()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
